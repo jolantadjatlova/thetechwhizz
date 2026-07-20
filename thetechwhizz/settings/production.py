b26387e54d93e1e97a9bf60b7dc90b632fa44deb
jolantadjatlova <jolanta.djatlova@icloud.com>
2026-07-20 16:24:12 +0100

    Fix static file handling with WhiteNoise and Cloudinary

--- thetechwhizz/settings/production.py
+++ thetechwhizz/settings/production.py
@@ -1,5 +1,7 @@
 import os
+
 import cloudinary
+
 from .base import *
 
 DEBUG = False
@@ -9,50 +11,88 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
+
     "https://thetechwhizz-a8e4bd1567a3.herokuapp.com",
+
 ]
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-WHITENOISE_MANIFEST_STRICT = False
+# WhiteNoise handles static files.
 
-# Cloudinary for media storage (Heroku's filesystem is ephemeral,
-# so uploaded images/media need to persist elsewhere)
-cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    secure=True,
+STORAGES["staticfiles"]["BACKEND"] = (
+
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 )
 
-STORAGES["default"]["BACKEND"] = "cloudinary_storage.storage.MediaCloudinaryStorage"
-STATICFILES_STORAGE = STORAGES["staticfiles"]["BACKEND"]
+# Compatibility setting for django-cloudinary-storage's collectstatic command.
+
+# Django itself uses STORAGES above.
+
+
+# Cloudinary handles uploaded media files.
+
+STORAGES["default"]["BACKEND"] = (
+
+    "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+)
+
+cloudinary.config(
+
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+
+    secure=True,
+
+)
 
 LOGGING = {
+
     "version": 1,
+
     "disable_existing_loggers": False,
+
     "handlers": {
+
         "console": {
+
             "class": "logging.StreamHandler",
+
         },
+
     },
+
     "root": {
+
         "handlers": ["console"],
+
         "level": "WARNING",
+
     },
+
     "loggers": {
+
         "django": {
+
             "handlers": ["console"],
+
             "level": "ERROR",
+
             "propagate": False,
+
         },
+
     },
+
 }
 
 try:
+
     from .local import *
+
 except ImportError:
+
     pass
